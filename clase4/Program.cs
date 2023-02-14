5970dc5afbad175cda421f90c9d68a7a6c5f6c16
BrayanEAA <brayaneduardoarteaga@gmail.com>
2023-02-13 20:19:03 -0600

    cambio en el titulo

--- clase4/Program.cs
+++ clase4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("programacion 1");
+Console.WriteLine("programacion ");
 
 Console.WriteLine("prueba");
